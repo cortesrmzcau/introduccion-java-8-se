8f4c3f38a4dd185348db17af67ccaa19b84920ee
cortesrmzcau <cesar.a.cortes.94@gmail.com>
2022-07-03 22:48:50 -0500

    Terminación del Curso

---
+++ src/FoorLoop.java
@@ -0,0 +1,20 @@
+public class FoorLoop {
+    static boolean isTurnOnLight = false;
+
+    public static void main(String[] args) {
+        turnOnOffLight();
+
+        for (int i = 0; i < 10; i++) {
+            printSOS();
+        }
+    }
+
+    public static void printSOS() {
+        System.out.println("...---...");
+    }
+
+    public static boolean turnOnOffLight() {
+        isTurnOnLight = (isTurnOnLight) ? false : true;
+        return isTurnOnLight;
+    }
+}
